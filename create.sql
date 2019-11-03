ef682e6dbad6365ddbb94924a977381041ffaa9e
Michael Beck <beckmd@miamioh.edu>
2019-11-03 16:22:57 -0500

    Updated database and dependencies

--- create.sql
+++ create.sql
@@ -41,19 +41,23 @@ CREATE TABLE order_line_customizations (
     customization_id integer REFERENCES customizations (id)
 );
 
-INSERT INTO customers VALUES (1, 'Michael Beck', 'beckmd', 'beckmd@miamioh.edu', '$2b$10$qtcZLq9ouJrwPn8jGSmcku1JKo2bWr9BGKH6LE37mll2ehdfIdFcS');
+INSERT INTO customers (name, username, email, password) VALUES ('Michael Beck', 'beckmd', 'beckmd@miamioh.edu', '$2b$10$qtcZLq9ouJrwPn8jGSmcku1JKo2bWr9BGKH6LE37mll2ehdfIdFcS');
 
-INSERT INTO products VALUES (2, 'Breadsticks', 'Garlic breadsticks and dipping sauce.', 'Sides', 'images/breadsticks.jpg', '$4.99');
-INSERT INTO products VALUES (1, 'Veggie Lovers', 'Run it through the garden!', 'Pizzas', 'images/veggie.jpg', '$12.99');
+INSERT INTO products (name, description, category, image, price) VALUES ('BYO', 'Build your own pizza!', 'BYO', 'images/cheese.jpg', '$9.99');
+INSERT INTO products (name, description, category, image, price) VALUES ('Veggie Lovers', 'Run it through the garden!', 'Pizzas', 'images/veggie.jpg', '$12.99');
+INSERT INTO products (name, description, category, image, price) VALUES ('Breadsticks', 'Garlic breadsticks and dipping sauce.', 'Sides', 'images/breadsticks.jpg', '$4.99');
 
-INSERT INTO customizations VALUES (1, 'Pepperoni', 'Meat Topping');
-INSERT INTO customizations VALUES (2, 'Jalepenos', 'Veggie Topping');
+INSERT INTO customizations (name, category) VALUES ('Pepperoni', 'Meat Topping');
+INSERT INTO customizations (name, category) VALUES ('Sausage', 'Meat Topping');
+INSERT INTO customizations (name, category) VALUES ('Jalepenos', 'Veggie Topping');
+INSERT INTO customizations (name, category) VALUES ('Mushrooms', 'Veggie Topping');
 
-INSERT INTO orders VALUES (1, 1, '2019-04-07 14:51:08.131249');
-INSERT INTO orders VALUES (2, 1, '2019-04-07 14:51:14.740154');
+INSERT INTO orders (customer_id, created_at) VALUES (1, '2019-04-07 14:51:08.131249');
+INSERT INTO orders (customer_id, created_at) VALUES (1, '2019-04-07 14:51:14.740154');
 
-INSERT INTO order_lines VALUES (1, 1, 1, 1, '$12.99');
-INSERT INTO order_lines VALUES (2, 2, 1, 1, '$12.99');
-INSERT INTO order_lines VALUES (3, 2, 2, 2, '$9.98');
+INSERT INTO order_lines (order_id, product_id, quantity, price) VALUES (1, 2, 1, '$12.99');
+INSERT INTO order_lines (order_id, product_id, quantity, price) VALUES (2, 1, 1, '$12.99');
+INSERT INTO order_lines (order_id, product_id, quantity, price) VALUES (2, 3, 2, '$4.99');
 
-INSERT INTO order_line_customizations VALUES (1, 2, 1);
+INSERT INTO order_line_customizations (order_line_id, customization_id) VALUES (2, 1);
+INSERT INTO order_line_customizations (order_line_id, customization_id) VALUES (2, 2);
