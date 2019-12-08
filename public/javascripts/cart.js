20d7b49e0629065ca17fd31b71dbb7cba976493a
XIAOGENG CHI <CHIX@MIAMIOH.EDU>
2019-12-08 12:17:28 -0500

    Tuinn-in

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -7,3 +7,19 @@ async function addCart (params) {
     document.querySelector('#cartCount').innerHTML = 'Cart (' + result.data.cartCount + ')';
 }
 
+async function addCustomizeToCart (params) {
+    const POST = {
+        "product_id": params,
+        "quantity": 1,
+        "customizations": []
+    };
+
+    const checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    for(i = 0; i < checkedBoxes.length;i++){
+        POST.customizations.push (checkedBoxes[i].value);
+        checkedBoxes[i].checked = false;
+    }
+    
+    const result = await axios.post('cart', POST);
+    document.querySelector('#cartCount').innerHTML = 'Cart (' + result.data.cartCount + ')';
+}
--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,12 @@ router.get('/', async (req, res) =>{
   const params = [];
   const result = await db.query(query, params);
 
-  console.log(req.session.customer, result);
-  res.render('index', {customer: req.session.user, title:'Pizza 330', rows: result.rows});
+  //-------- Customize
+  let queryC = 'SELECT * FROM customizations';
+  const paramsC = [];
+  const resultC = await db.query(queryC, paramsC);
+
+  res.render('index', {customer: req.session.user, title:'Pizza 330', rows: result.rows, rowsC: resultC.rows});
 });
   
 //-------- Add to cart
@@ -29,44 +33,20 @@ router.post('/cart', async (req, res) =>{
     quantity: req.body.quantity
   };
 
-  req.session.cart.push(itemToAdd);
-  req.session.nextCartId += 1;
-  req.session.cartCount += req.body.quantity;
+  if(req.body.customizations){
+    itemToAdd.customizations = [];
+     for(i =0; i < req.body.customizations.length; i++){
+      let query2 = 'SELECT * FROM customizations WHERE id = $1';
+      const params2 = [ req.body.customizations[i] ];
 
-  res.json({cartCount: req.session.cartCount, cart:req.session.cart});
-});
-
-//-------- Customize
-router.get('/', async (req, res) =>{
-  let query = 'SELECT * FROM customizations';
-  const params = [];
-  const result = await db.query(query, params);
-
-  console.log(req.session.customize, result);
-  res.render('index', {customize: req.session.user, rows: result.rows});
-});
-
-router.post('/cart', async (req, res) =>{
-  let query = 'SELECT * FROM customizations WHERE id = $1';
-  const params = [
-    req.body.product_id
-  ];
-  const result = await db.query(query, params);
-
-  const checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
-
-  const itemToAdd ={
-    cart_id : req.session.nextCartId,
-    product_id: req.body.product_id,
-    name: checkedBoxes[0].value,
-    quantity: req.body.quantity
-  };
+      const result2 = await db.query(query2, params2);
+      itemToAdd.customizations.push({id: req.body.customizations[i], name: result2.rows[0].name});
+    }
+  }
 
   req.session.cart.push(itemToAdd);
   req.session.nextCartId += 1;
   req.session.cartCount += req.body.quantity;
-
-  res.json({cartCount: req.session.cartCount, cart:req.session.cart});
 
   res.json({cartCount: req.session.cartCount, cart:req.session.cart});
 });
