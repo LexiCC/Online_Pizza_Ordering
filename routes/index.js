994d53dac74bf7d97539e2f9361581feecf6b295
fuh4 <fuh4@miamioh.edu>
2019-12-08 16:09:09 -0500

    Fixing the cart summary

--- routes/index.js
+++ routes/index.js
@@ -4,43 +4,45 @@ const db = require('../db');
 const router = express.Router();
 
 
-//-------- Menu
-router.get('/', async (req, res) =>{
-  let query = 'SELECT * FROM products';
+// -------- Menu
+router.get('/', async (req, res) => {
+  const query = 'SELECT * FROM products';
   const params = [];
   const result = await db.query(query, params);
 
-  //-------- Customize
-  let queryC = 'SELECT * FROM customizations';
+  // -------- Customize
+  const queryC = 'SELECT * FROM customizations';
   const paramsC = [];
   const resultC = await db.query(queryC, paramsC);
 
-  res.render('index', {customer: req.session.user, title:'Pizza 330', rows: result.rows, rowsC: resultC.rows});
+  res.render('index', {
+    customer: req.session.user, title: 'Pizza 330', rows: result.rows, rowsC: resultC.rows,
+  });
 });
-  
-//-------- Add to cart
-router.post('/cart', async (req, res) =>{
-  let query = 'SELECT * FROM products WHERE id = $1';
+
+// -------- Add to cart
+router.post('/cart', async (req, res) => {
+  const query = 'SELECT * FROM products WHERE id = $1';
   const params = [
-    req.body.product_id
+    req.body.product_id,
   ];
   const result = await db.query(query, params);
 
-  const itemToAdd ={
-    cart_id : req.session.nextCartId,
+  const itemToAdd = {
+    cart_id: req.session.nextCartId,
     product_id: req.body.product_id,
     name: result.rows[0].name,
-    quantity: req.body.quantity
+    quantity: req.body.quantity,
   };
 
-  if(req.body.customizations){
+  if (req.body.customizations) {
     itemToAdd.customizations = [];
-     for(i =0; i < req.body.customizations.length; i++){
-      let query2 = 'SELECT * FROM customizations WHERE id = $1';
-      const params2 = [ req.body.customizations[i] ];
+    for (i = 0; i < req.body.customizations.length; i++) {
+      const query2 = 'SELECT * FROM customizations WHERE id = $1';
+      const params2 = [req.body.customizations[i]];
 
       const result2 = await db.query(query2, params2);
-      itemToAdd.customizations.push({id: req.body.customizations[i], name: result2.rows[0].name});
+      itemToAdd.customizations.push({ id: req.body.customizations[i], name: result2.rows[0].name });
     }
   }
 
@@ -48,14 +50,12 @@ router.post('/cart', async (req, res) =>{
   req.session.nextCartId += 1;
   req.session.cartCount += req.body.quantity;
 
-  res.json({cartCount: req.session.cartCount, cart:req.session.cart});
+  res.json({ cartCount: req.session.cartCount, cart: req.session.cart });
 });
 
 module.exports = router;
 
-//-----------cart summary
-router.get('/cart', async (req, res) =>{
-  
-
-  res.render('cart');
+// -----------cart summary
+router.get('/cart', async (req, res) => {
+  res.render('cart', { cart: req.session.cart, customer: req.session.customer, cartCount: req.session.cartCount });
 });
